feca3877957da1be1b8bcb01f4577d00115f160f
zanko <zankonorii@gmail.com>
2022-04-05 00:00:47 +0430

    adding channel and print channel value

--- main.go
+++ main.go
@@ -10,15 +10,23 @@ const (
 	perTime = 5
 )
 
+type website struct{
+	name string
+	url string
+	status int 
+}
+
+var channel = make (chan website)
+
 func doEvery(d time.Duration, f func(time.Time)) {
 	for x := range time.Tick(d) {
 		f(x)
+		printStatus()
 	}
 }
 
 
 func main() {
-	
 	doEvery(perTime*time.Second, checkUrlLoop)
 }
 
@@ -26,22 +34,26 @@ func checkUrlLoop(t time.Time) {
 	fmt.Println("Checking urls...")
 	urls := returnUrls()
 
-	for name, url := range urls {
-		go checkUrl(url, name)
+	for _, website := range urls {
+		go checkUrl(website)
 	}
 }
 
-func checkUrl(url string, name string) {
-	response, err := http.Get(url)
+func checkUrl(website website) {
+	response, err := http.Get(website.url)
 
-	if err != nil || response.StatusCode%2 != 0 {
-		fmt.Println(name + " is Down")
+	if err != nil {
+		website.status = -1
 	} else {
-		fmt.Println(name + " is up")
+		website.status = response.StatusCode
 	}
+
+	channel <- website
 }
 
-func returnUrls() map[string]string {
+func returnUrls() []website {
+
+	//we can remove this later
 	urls := make(map[string]string)
 
 	urls["golang"] = "https://go.dev/"
@@ -53,5 +65,23 @@ func returnUrls() map[string]string {
 	urls["postgres"] = "https://www.postgresql.org/"
 	urls["haskel"] = "https://www.haskel.com/en-us"
 
-	return urls
+
+	websites := []website{}
+
+	for name, url := range urls{
+		websites = append(websites, website{name, url, 0})
+	}
+
+	return websites
+}
+
+func printStatus(){
+	for i := 0; i < len(returnUrls()); i++ {
+		website := <- channel
+		if website.status / 100 == 2 {
+			fmt.Println("✅ ", website.name)
+		}else{
+			fmt.Println("❌", website.name)
+		}
+	}
 }
